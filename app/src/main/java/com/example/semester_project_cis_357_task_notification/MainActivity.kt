52677135367a6644cf0e068aa384a437e107dfd4
Hoang Nguyen <nguyenh1@mail.gvsu.edu>
2024-11-03 22:21:51 -0500

    Initial commit of Android Studio project

---
+++ app/src/main/java/com/example/semester_project_cis_357_task_notification/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.semester_project_cis_357_task_notification
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.semester_project_cis_357_task_notification.ui.theme.Semesterprojectcis357tasknotificationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Semesterprojectcis357tasknotificationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Semesterprojectcis357tasknotificationTheme {
+        Greeting("Android")
+    }
+}
